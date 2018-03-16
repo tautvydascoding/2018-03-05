9359a3708a05dafcddee43802cd6cbe74314e9ca
gyciakas <gyciakas@gmail.com>
2018-03-16 17:05:20 +0200

    penktad-pask

---
+++ dirbame_cia/diena_10/gytisc/04-jquery/main.js
@@ -0,0 +1,12 @@
+console.log( " Labas ");
+
+
+// $('h1').hide(  3000 );
+
+$(".slepti").click(function() {
+  $('h1').hide(  3000 ); // 3000 - yra 3 sek, per kurias dingsta elementas su klase . slepti
+});
+
+$("p").click(function() {
+  $('h1').hide(  3000 );
+});
