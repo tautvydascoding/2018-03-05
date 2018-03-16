f72fc672f11e0a134d25fe54bb1bd4dbdbe5008c
grajauskas <e.grajauskas@gmail.com>
2018-03-16 12:38:03 +0200

    diena 10.2

--- dirbame_cia/diena_10/destytojas/2-js-DOM-querySelector-naudojimas/main.js
+++ dirbame_cia/diena_10/destytojas/2-js-DOM-querySelector-naudojimas/main.js
@@ -1,6 +1,6 @@
 console.log( " Labas ");
 
-var aprasymas = document.querySelector(  '.straipsnis-1' );
+var aprasymas = document.querySelector(  '.straipsnis-1' );  // Ne array, o vienas elementas
 console.log(  aprasymas  );
 aprasymas.innerHTML  = "Pakeistas su js";
 
@@ -11,5 +11,16 @@ console.log( visiStraipsniai );
 visiStraipsniai[2].innerHTML = "Pakeistas su masyvo pagalba";
 
 for (var i = 0; i < 3; i++) {  // visiStraipsniai.length
-    visiStraipsniai[i].innerHTML  =  "Pakeistas us FOR ciklu";
+    visiStraipsniai[i].innerHTML  =  "Pakeistas us FOR ciklu";  // keiciam teksuts
+    visiStraipsniai[i].style.color = 'blue';  // keiciam dizaina / CSS
+    visiStraipsniai[i].style.backgroundColor = '#f1f1f1' ;
 }
+// ================ keiciam spalva h2 ir p
+var  visiTekstai = document.querySelectorAll( ' h2,  p' );  // gauvom array
+
+console.log(  visiTekstai  );
+
+for (var i = 0; i < 6; i++) { //  visiTekstai.length
+    visiTekstai[i].style.color = "red";
+}
+//
