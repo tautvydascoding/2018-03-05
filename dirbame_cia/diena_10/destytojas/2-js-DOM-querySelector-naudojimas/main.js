5c0058a00d6ae110e64027ede0938f85871884e1
grajauskas <e.grajauskas@gmail.com>
2018-03-16 12:12:05 +0200

    diena 10.2

--- dirbame_cia/diena_10/destytojas/2-js-DOM-querySelector-naudojimas/main.js
+++ dirbame_cia/diena_10/destytojas/2-js-DOM-querySelector-naudojimas/main.js
@@ -1,4 +1,15 @@
 console.log( " Labas ");
 
+var aprasymas = document.querySelector(  '.straipsnis-1' );
+console.log(  aprasymas  );
+aprasymas.innerHTML  = "Pakeistas su js";
 
-//
+//====================keiciam visus straipnsnius
+var visiStraipsniai = document.querySelectorAll( 'p'); // gauname Array
+
+console.log( visiStraipsniai );
+visiStraipsniai[2].innerHTML = "Pakeistas su masyvo pagalba";
+
+for (var i = 0; i < 3; i++) {  // visiStraipsniai.length
+    visiStraipsniai[i].innerHTML  =  "Pakeistas us FOR ciklu";
+}
