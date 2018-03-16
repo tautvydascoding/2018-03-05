21f37d62827e00355eff12bc96e252de3cfc18d8
gyciakas <gyciakas@gmail.com>
2018-03-16 16:39:19 +0200

    klas

--- dirbame_cia/diena_10/gytisc/03-js-for/main.js
+++ dirbame_cia/diena_10/gytisc/03-js-for/main.js
@@ -36,7 +36,76 @@ for (var i = 0; i <  names.length; i++) {
 }
 
 function ieskotiVienoVardo() {
-    var ieskome = "Rico";
+    var ieskome = "Rico----";
+
+    for (var i = 0; i <  names.length; i++) {
+        // tikrinu ar stalcius yra "Rico"
+        if (  names[i] == ieskome ) {
+            console.log("Raudau pirmaji Rico",  i );
+            break;
+        }
+    }
+}
+ieskotiVienoVardo();
+// 1A ----
+    var atlyginimas = 500;
+    // UZDUOTIS
+    // A) patikrinti ar 'atlyginimas' padidejo. Jeigu padidejo pranesti. jeigu nepasikeite - irgi pranesti
+    // B) padidinti 'atlyginimas' vienu simtu. Ir vel patikrinti ar 'atlyginimas' padidejo
+
+
+
+// 1B) papildyti ^: jeigu tokio vardo neranda, isvesti VIENĄ pranesima "Nepavyko rasti...Bandykite kita zodi"
+
+function ieskotiVienoVardo222() {
+    var ieskome = "Rico";  // local
+    var arRadau = "neradau";      // local   "neradau"
+    for (var i = 0; i <  names.length; i++) {
+        // tikrinu ar stalcius yra "Rico"
+        if (  names[i] == ieskome ) {
+            console.log("Raudau pirmaji Rico",  i );
+            arRadau = "suradau"; // "radau"
+            break;
+        }
+    }
+
+
+    // tikriname ar pavyko rasti
+    if (arRadau == "neradau") { // "neradau"
+        console.log( " nepavyko rasti ");
+    } else {
+        console.log( " suradau ");
+    }
+}
+ieskotiVienoVardo222();
+
+if (atlyginimas > 500) {
+  asPasikeis = true; //
+}
+
+// 2 UZDUOTIS (f-ja iekom stalciaus)
+// parasyti funkcija, kuriai davus iekoma zodi , ji suranda jo vieta masyve (stalciaus numeri) ir si   numeri grazina
+// eg: getStalciausNumeris( ieskomasTekstas)
+
+// uzduots:----------------------------
+// 3) rasti pavarde masyve esancio  zmogaus vardu "Freida" (pirmojo)
+
+var ilgis = names.length;
+console.log( "vardu skaicius:" + ilgis );
+
+var nr = 0;
+var ieskome = "Freida";
+
+for (var i = 0; i <  names.length; i++) {
+    // tikrinu ar stalcius yra "Freida"
+    if (  names[i] == ieskome ) {
+        console.log("Rauda""+ieskome Freida", nr, i );
+    }
+    nr++;
+}
+
+function ieskotiVienoVardo() {
+    var ieskome = "Rico----";
 
     for (var i = 0; i <  names.length; i++) {
         // tikrinu ar stalcius yra "Rico"
@@ -48,19 +117,6 @@ function ieskotiVienoVardo() {
 }
 ieskotiVienoVardo();
 
-// 1B) papildyti ^: jeigu tokio vardo neranda, isvesti VIENĄ pranesima "Nepavyko rasti...Bandykite kita zodi"
-
-var neranda = "pranesimas";
-if (ieskotiVienoVardo == "Rico") {
-} else {
-  console.log("Nepavyko rasti...Bandykite kita zodi");
-}
-// 2 UZDUOTIS (f-ja iekom stalciaus)
-// parasyti funkcija, kuriai davus iekoma zodi , ji suranda jo vieta masyve (stalciaus numeri) ir si   numeri grazina
-// eg: getStalciausNumeris( ieskomasTekstas)
-
-// uzduots:----------------------------
-// 3) rasti pavarde masyve esancio  zmogaus vardu "Freida" (pirmojo)
 // 4) rasti visu zmoniu vardu "Rico" pavardes
 // 5) Turime masyva su zmoniu nr.  ieskomiZmones = [2, 16, 17, 18, 19, 25];  isvesti ju pavardes ir vardus
 
