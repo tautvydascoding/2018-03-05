54d2a17942cb0b9a388221e003941dfa77cb36e7
grajauskas <e.grajauskas@gmail.com>
2018-03-19 12:39:28 +0200

    diena 11.1

--- dirbame_cia/diena_11/destytojas/2-js--elementu-kurimas/main.js
+++ dirbame_cia/diena_11/destytojas/2-js--elementu-kurimas/main.js
@@ -1,1 +1,18 @@
 console.log( " Labas ");
+
+
+$('.kurejas').click(function( ) {
+    // document.write(" sukuriu <br> ");
+
+    // sukuriu <article> </article>
+    var straipsnis = document.createElement("article");
+    var tekstas = document.createTextNode(" Raudonkepuraite ejo i miska...");
+    // console.log( straipsnis ); //test ar veikia
+    // console.log( tekstas ); //test ar veikia
+
+    straipsnis.appendChild(  tekstas  );  // <article>  Raudonkepuraite ejo i miska...</article>
+    document.querySelector('section').appendChild(  straipsnis );
+
+    //arba
+    document.querySelector('section').innerHTML += "<article>  Raudonkepuraite ejo..</article>";
+});
