04c54b4df88a3c9662c3ee098b5e3671b1718eda
grajauskas <e.grajauskas@gmail.com>
2018-03-19 14:18:16 +0200

    diena 11.3

--- dirbame_cia/diena_11/destytojas/2-js--elementu-kurimas/main.js
+++ dirbame_cia/diena_11/destytojas/2-js--elementu-kurimas/main.js
@@ -16,3 +16,36 @@ $('.kurejas').click(function( ) {
     //arba
     document.querySelector('section').innerHTML += "<article>  Raudonkepuraite ejo..</article>";
 });
+
+
+
+
+
+// ============================
+
+
+// 1. sukurti "header"  DOM objekta/NODE'a
+// test
+var manoHeader = document.createElement('header');
+// console.log(  manoHeader );
+
+// 2. sukurti 'tekstas' kintamaji ir sukurti teksto Node : panaudojant js document.createTextNode(xxx)
+
+var tekstas = document.createTextNode(' Manno tekstas');
+// console.log(  tekstas ); // test
+
+// 3. i susikuta 'header' objekta idedame 'tekstas' objekta
+manoHeader.appendChild(  tekstas );  // appendChild - ideda i header'io kita elementa 'tekstas'
+console.log(  manoHeader );        // test
+
+// 4. headeri idesime virs H1
+// insertBefore(kaIdeti, PriesKa) -
+var manoH1 = document.querySelector('h1');  // surandam pirma h1
+
+  // tetis elemet'o (h1), kurio atzvilgiu        kaIdeti     KienoAtzvilgiu (pries ka)
+document.querySelector('body').insertBefore(  manoHeader , manoH1 );
+
+
+
+
+//
