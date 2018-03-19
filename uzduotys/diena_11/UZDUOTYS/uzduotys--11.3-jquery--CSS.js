756395114d9be3fd1628858205762cade75f36d6
grajauskas <e.grajauskas@gmail.com>
2018-03-19 15:21:01 +0200

    diena 11.3

--- uzduotys/diena_11/UZDUOTYS/uzduotys--11.3-jquery--CSS.js
+++ uzduotys/diena_11/UZDUOTYS/uzduotys--11.3-jquery--CSS.js
@@ -10,7 +10,7 @@
 // <h2> 2. h2 Isijunk konsole</h2>
 // <h2> 3. h2 Isijunk konsole</h2>
 
-//  <section> 
+//  <section>
 //      <h2> h2 Isijunk konsole</h2>
 //      <h2> h2 Isijunk konsole</h2>
 //      <h2> h2 Isijunk konsole</h2>
@@ -20,13 +20,16 @@
 // <ul>
 //     <li>Home</li>
 //     <li>About</li>
-//     <li classs='reklama'>Gallery</li>
+//     <li class="reklama">Gallery</li>
 //     <li>Contact</li>
 // </ul>
-//        <input type="text" name="vardas" value="">
-//        <input type="email" name="pastas" value="">
+
+//  <input type="text" name="vardas" value="">
+//  <input type="email" name="pastas" value="">
+// <!-----end html-------------------------->
+
 
 // A) visus h1 ir h2 pakeisti i melyna spalva (visur naudoti tik jQuery)
-// A2) kas antra h2 pakeisti i melyna 
+// A2) kas antra h2 pakeisti i melyna
 // B) paskutinta "li" elemnta pakeisti i zalia (visur naudoti tik jQuery)
-// C) visus 'li' elementus pakeisti i geltonus (isskyrus su klase 'reklama') pvz.: li:not('.reklama') {}	
+// C) visus 'li' elementus pakeisti i geltonus (isskyrus su klase 'reklama') pvz.: li:not('.reklama') {}
