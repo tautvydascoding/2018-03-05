3bc58e4224589d54fe0a8867a125320289a44092
grajauskas <e.grajauskas@gmail.com>
2018-03-20 16:30:05 +0200

    diena 12.2

--- uzduotys/diena_12/UZDUOTYS/uzduotys--12.5-jquery-get-set-text.js
+++ uzduotys/diena_12/UZDUOTYS/uzduotys--12.5-jquery-get-set-text.js
@@ -2,14 +2,15 @@
 
 
 // .html kodas:
+//
 // <h1>Isijunk konsole</h1>
 // <h1>Isijunk konsole</h1>
 //
 // <h2> 1. h2 Isijunk konsole</h2>
 // <h2> 2. h2 Isijunk konsole</h2>
 // <h2> 3. h2 Isijunk konsole</h2>
-
-//  <section> 
+//
+//  <section>
 //      <h2> 1. h2 Isijunk konsole</h2>
 //      <h2> 2. h2 Isijunk konsole</h2>
 //      <h2> 3. h2 Isijunk konsole</h2>
@@ -19,17 +20,23 @@
 // <ul>
 //     <li>Home</li>
 //     <li>About</li>
-//     <li classs='reklama'>Gallery</li>
+//     <li class="reklama">Gallery</li>
 //     <li>Contact</li>
 // </ul>
+
+
+        // <form  action="#" method="post">
+        //     <input type="text" name='vardas'  value="" placeholder="Jusu vardas" >
+        //     <input type="email" name='pastas' value="" placeholder="Jusu pastas" >
+        // </form>
 
 // UZDUOTIS 1
 // pakeisti visus h1 tekstus i "Antraste nr: XX" ( vietoj "xx" isvesti skaiciu, kelinta tai antrate)
 
 //  UZDUOTIS 2
-// pakeisti visus h2 tekstus, kurie yra "section" elemente. 
-// pakeisti i "Pakeista antraste "  
-// 
+// pakeisti visus h2 tekstus, kurie yra "section" elemente.
+// pakeisti i "Pakeista antraste "
+//
 
 // UZDUOTIS 3
 // i console atspausdinti "li" elemento, kurio klase "reklama" - teksta
@@ -56,6 +63,3 @@
 //  visiems "input" elementams pridete atributa: value=' xxx '   (naujant jQuery)
 //  ivesti vardas: 'Tomas'
 //  ivesti pastas: 'a@a.lt'
-
-
-	
