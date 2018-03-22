a562b9a2501b7de1dbd219329521f7a395ce01a6
Tautvydas <arttgd@gmail.com>
2018-03-22 22:36:04 +0200

    15d

---
+++ uzduotys/diena_15/UZDUOTYS/uzduotys-1-sql-pasikartoti-db-kurima.php
@@ -0,0 +1,5 @@
+// UZDUOTIS
+// Pasikartoti DB kurima
+// 1. Sukurti DB 'blogas'
+// 2. sukurti table/lentele 'straipsniai'  id, title, description, date, author
+// 3. ideti pora straipnsiu  (ne Lorem)
