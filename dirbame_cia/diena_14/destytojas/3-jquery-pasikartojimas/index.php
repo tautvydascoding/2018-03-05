afcfcbf5dd572b7468c4da6a688f4343fa6e47be
grajauskas <e.grajauskas@gmail.com>
2018-03-22 16:53:59 +0200

    14.1

---
+++ dirbame_cia/diena_14/destytojas/3-jquery-pasikartojimas/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <!-- reikalinga kad prisitaikantis dizainas veiktu tvarkingai -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.min.css">
+        <!-- galimos klaidos -->
+        <!-- blogas kelias iki failo -->
+        <!-- "/" ne i ta puse -->
+        <!-- neuzdarete ">" -->
+        <link rel="stylesheet" href="css/master.css">
+        <!-- !!!! VISSA MANO CSS failas pats zemiausias -->
+    </head>
+
+    <body>
+
+
+        <div class="container">
+            <header >
+                 <h1> 1 header </h1>
+                 <h2> 2 header </h2>
+                 <h3> 3 header </h3>
+                 <h4> 4 header </h4>
+            </header>
+
+            <footer>
+                 <h1> 1 footer </h1>
+                 <h2> 2 footer </h2>
+                 <h3> 3 footer </h3>
+                 <h4> 4 footer </h4>
+            </footer>
+        </div>
+
+
+
+
+        <!-- bootstrap4 -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- !!! jQuery "slim" yra nepilna versija ir jQuery kai kurios komandos neveikia, reiktu isisdeti kita jQuery  -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <!-- !!! mano js failas visada zemiausias -->
+        <script type="text/javascript" src="main.js"> </script>
+    </body>
+</html>
