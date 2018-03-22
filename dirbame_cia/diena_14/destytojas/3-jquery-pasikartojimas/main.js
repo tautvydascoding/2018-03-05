1bee01e20e87503eaf03daa3252aceda084ace26
grajauskas <e.grajauskas@gmail.com>
2018-03-22 17:00:32 +0200

    14.1

--- dirbame_cia/diena_14/destytojas/3-jquery-pasikartojimas/main.js
+++ dirbame_cia/diena_14/destytojas/3-jquery-pasikartojimas/main.js
@@ -1,1 +1,17 @@
 console.log( " Labas ");
+
+
+
+
+
+
+// UZDUOTYS PRIES TESTAS JS IR JQUERY
+// 1. su js  pakeisti 'header' H1 teksta i " Pakeistas su js"
+// 2. su jQury  pakeisti 'footer' H2 teksta i " Pakeistas su JQ"
+// 3. su jQuery paimti  'footer' h1 tekta ir atspaudinti i console
+
+
+
+// UZDUOTYS
+// 1. su jQury sukurti elementa 'section' su tekstu 'mano tekstas'
+// 2. su jQury   'section' ideti tarp 'header' ir 'footer'
