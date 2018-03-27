927668f9b679fbe63fe1b212881b65bea91161eb
grajauskas <e.grajauskas@gmail.com>
2018-03-27 11:01:35 +0300

    17.1

--- dirbame_cia/diena_17/destytojas/1-PHP-FOR--while-foreach/index.php
+++ dirbame_cia/diena_17/destytojas/1-PHP-FOR--while-foreach/index.php
@@ -5,7 +5,7 @@
 // 1) sukurti masyva : 1981, 1980, 1975, 1995, 1999
 // 1.1) atspausdinti masyva naudojant FOR cikla, While cikla ir FOREACH cikla
 // 1.4 Atspausdinti kas antra data
-// 1.5 Atspasudinti visas datas nuo 3-cios
+// 1.5 Atspasudinti visas datas nuo 3-cios (iskaitant 3-cia)
 
 // 2) sukurti matirca:
 // Tomas, 1981, Buhalteris
@@ -27,3 +27,29 @@ endfor;
 foreach ($masyvas as $stalciausPavad => $stalciausTurinys) {
     echo "Gimimo data su FOREACH $stalciausPavad:   $stalciausTurinys  <br>";
 }
+// 1.1.3
+$kk = 0;
+while ( $kk <  5 ) {
+    echo "While gimimo datos: " .   $masyvas[ $kk ] . "<br>";
+     $kk++;
+
+     // jei skaiciuuoju/ dirbu be galo
+     if($kk > 10000) {
+         break; // nutraukiu while cikla
+     }
+}
+// 1.4 Atspausdinti kas antra data
+for ($i=0; $i < 5;   $i = $i + 2  ) {
+    echo "Kas antra data: " .  $masyvas[ $i ] . "<br>";
+}
+// ARBA
+// for ($i=0; $i < 5 ; $i++) {
+//     echo "Kas antra data: " .  $masyvas[ $i ] . "<br>";
+//     $i++;
+// }
+for ($i=0, $tt=100     ; $i < 5;      $i++, $tt = $tt +2 ) {
+    echo " i: $i ir tt: $tt <br />"  ;
+}
+// 'i' ir kam "tt"
+//  0         100
+//
