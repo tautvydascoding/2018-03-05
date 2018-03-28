7e12786430243b75805f3e9aa89e58b243aab3fd
Jevgenijus <jborcevskij@gmail.com>
2018-03-29 01:28:15 +0300

    projektas...

---
+++ dirbame_cia/VIOSI-PROJEKTAI/Jevgenijus_projektas/Design_bootstrap_layouts_DarbinisFolderis/layouts/php/header.php
@@ -0,0 +1,77 @@
+<div class="container">
+
+
+<!-- Headeris -->
+<header class="row  headeris aukstis-230">
+<div class="col-md">
+
+<!-- Logotipas -->
+<a class="logotipas" href="/index.html"><img src="img/logo.png" alt="Logotipas"></a>
+
+<!-- Prisijungimas -->
+<form class="form-group float-right">
+<label class="sr-only" for="inlineFormInputName2">Name</label>
+<input type="text" class="form-control mb-2 mr-md-1" id="inlineFormInputName2" placeholder="Prisijungimo vardas">
+
+<label class="sr-only" for="inlineFormInputGroupUsername2">Prisijungimas</label>
+<div class="input-group mb-2 mr-md-1">
+<div class="input-group-prepend">
+</div>
+<input type="password" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Slaptažodis">
+</div>
+
+<div class="form-check mb-2 mr-md-1">
+<input class="form-check-input" type="checkbox" id="inlineFormCheck">
+<label class="form-check-label" for="inlineFormCheck">
+Prisiminti mane
+</label>
+</div>
+
+<button type="submit" class="btn btn-primary mb-2">Prisijungti</button>
+</form>
+
+
+<!-- Meniu pagrindinis -->
+<div class="row pagrindinis-meniu rounded aukstis-40">
+
+<ul class="nav nav-pills">
+<li class="nav-item">
+<a class="nav-link" href="#">Pagrindinis</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Meniu</a>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Straipsniai</a>
+<div class="dropdown-menu">
+<a class="dropdown-item" href="#">Šunys</a>
+<a class="dropdown-item" href="#">Katės</a>
+<a class="dropdown-item" href="#">Graužikai</a>
+<a class="dropdown-item" href="#">Paukščiai</a>
+<a class="dropdown-item" href="#">Akvariumai</a>
+<a class="dropdown-item" href="#">Ropliai</a>
+<a class="dropdown-item" href="#">Įdomybės</a>
+<div class="dropdown-divider"></div>
+<a class="dropdown-item" href="#">Visi straipsniai</a>
+</div>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Fotogalerija</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#">Videogalerija</a>
+</li>
+</ul>
+
+<!-- Paieska -->
+<form action="" method="post" class="search">
+<input type="search" name="" placeholder="Paieška" class="input" />
+<input type="submit" name="" value="" class="submit" />
+</form>
+</div>
+<!-- Paieska pabaiga -->
+</div>
+<!-- Meniu pagrindinis pabaiga -->
+
+</header>
+<!-- headeris end -->
---
+++ dirbame_cia/VIOSI-PROJEKTAI/Jevgenijus_projektas/Design_bootstrap_layouts_DarbinisFolderis/layouts/php/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include ("./head.php");
+include ("./header.php");
+include ("./nav.php");
+include ("./main.php");
+include ("/footer.php");
+
+
+ ?>
