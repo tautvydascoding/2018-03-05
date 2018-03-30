435e8ed8568f2cdecbc1c1f3f12202b1aaa9305e
SimonasSu <simon.s@inbox.lt>
2018-03-30 17:20:17 +0300

    13-20d +-

--- dirbame_cia/VIOSI-PROJEKTAI/Simonas_Projektas/Projektas/main.js
+++ dirbame_cia/VIOSI-PROJEKTAI/Simonas_Projektas/Projektas/main.js
@@ -1,1 +1,13 @@
-console.log( " Labas ");
+var video = document.getElementById("myVideo");
+function bgvideo(){
+
+}
+
+$(window).on('scroll', function() {
+  if($(window).scrollTop()) {
+    $('nav').addClass('black');
+  }
+    else {
+      $('nav').removeClass('black');
+    }
+  });
