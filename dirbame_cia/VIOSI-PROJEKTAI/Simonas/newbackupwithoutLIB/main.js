5d8690e9521fad4f88ab621513a88b326c55b0a0
SimonasSu <simon.s@inbox.lt>
2018-04-02 18:59:53 +0300

    28.1%

--- dirbame_cia/VIOSI-PROJEKTAI/Simonas/newbackupwithoutLIB/main.js
+++ dirbame_cia/VIOSI-PROJEKTAI/Simonas/newbackupwithoutLIB/main.js
@@ -1,13 +1,24 @@
-var video = document.getElementById("myVideo");
-function bgvideo(){
+$(window).scroll(function() {
+  parallax();
+});
 
+function parallax() {
+
+  var wScroll = $(window).scrollTop();
+
+$('.parallax--box').css('top',
+-5+(wScroll*0.0175)+'em');
+// Who we are blocko automatinis pavaziavimas efektas
+// -5+ nes uzdejau 5em css,  *0.0175 scroll greitis
 }
 
-$(window).on('scroll', function() {
-  if($(window).scrollTop()) {
-    $('nav').addClass('black');
-  }
-    else {
-      $('nav').removeClass('black');
-    }
-  });
+// var windowWidth = $(window).width();
+//
+// $('.container').mousemove(function(event) {
+//
+//     var moveX = (($(window).width() / 2) - event.pageX) * 0.1;
+//     var moveY = (($(window).height() / 2) - event.pageX) * 0.1;
+//     $('.footer').css('margin-left' , moveX + 'px');
+//     $('.footer').css('margin-top' , moveY + 'px');
+// });
+// failed effect above :'(
